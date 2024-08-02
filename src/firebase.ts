b722c2fb56b3e02744802571779d91af4903e052
techdevelopers08 <ajay@techangouts.com>
2024-08-02 09:03:50 -0400

    set firestore as development

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,6 +1,6 @@
     import { initializeApp } from 'firebase/app';
-    import { getFirestore } from 'firebase/firestore';
-    import { getAuth } from 'firebase/auth';
+    import { getFirestore,connectFirestoreEmulator } from 'firebase/firestore';
+    import { getAuth,connectAuthEmulator } from 'firebase/auth';
 
     const fetchFirebaseConfig = async () => {
     try {
@@ -24,6 +24,11 @@
     const app = initializeApp(firebaseConfig);
     const db = getFirestore(app);
     const auth = getAuth(app);
+
+    if (process.env.NODE_ENV === 'development') {
+    connectFirestoreEmulator(db, 'localhost', 8080);
+    connectAuthEmulator(auth, 'http://localhost:9099');
+    }
 
     return { db, auth };
     };
